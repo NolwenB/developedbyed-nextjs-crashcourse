aadcccef7a8da9f67fe53e6520682124650abd97
Nolwen <nbachtle@outlook.com>
2023-02-18 19:46:11 -0500

    Added TMDB

---
+++ app/Movie.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image"
+
+export default function Movie({ key, id, title, release_date, poster_path }) {
+    const imagePath = `https://image.tmdb.org/t/p/original/`;
+    console.log(poster_path);
+    return (
+        <div>
+            <h1>{title}</h1>
+            <h3>{release_date}</h3>
+            <Link href="/asd">
+                <Image
+                    src={imagePath + poster_path}
+                    alt={title}
+                    width={400}
+                    height={600}
+                />
+            </Link>
+        </div>
+    );
+}
