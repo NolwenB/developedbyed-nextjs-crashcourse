5001754624bae483cfbb44a93ebc92fc47111c7e
Nolwen <nbachtle@outlook.com>
2023-02-20 23:35:07 -0500

    Functional Movie Detail Page

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -3,12 +3,11 @@ import Image from "next/image"
 
 export default function Movie({ key, id, title, release_date, poster_path }) {
     const imagePath = `https://image.tmdb.org/t/p/original/`;
-    console.log(poster_path);
     return (
         <div>
             <h1>{title}</h1>
             <h3>{release_date}</h3>
-            <Link href="/asd">
+            <Link href={`/${id}`}>
                 <Image
                     src={imagePath + poster_path}
                     alt={title}
--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,7 +1,23 @@
-export default async function MovieDetail() {
-    return (
-        <div>
-            <h1>Movie Detail</h1>
-        </div>
-    )
+import Image from "next/image"
+
+export default async function MovieDetail({params}) {
+  console.log(params);
+  const imagePath = "https://image.tmdb.org/t/p/original";
+  const data = await fetch(`https://api.themoviedb.org/3/movie/${params.movie}?api_key=${process.env.API_KEY}`);
+  const res = await data.json();
+  const {id, title, release_date, poster_path } = res;
+  console.log(res);
+  return (
+    <div>
+      <h1>Movie detail</h1>
+      <h1>{title}</h1>
+            <h3>{release_date}</h3>
+                <Image
+                    src={imagePath + poster_path}
+                    alt={title}
+                    width={800}
+                    height={1200}
+                />
+    </div>
+  )
 }
