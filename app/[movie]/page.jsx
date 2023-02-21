00fee9daa8870c2d505b35c63b1ea24f86f51c7a
Nolwen <nbachtle@outlook.com>
2023-02-21 00:02:19 -0500

    Improved Movie Detail Page

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -5,19 +5,21 @@ export default async function MovieDetail({params}) {
   const imagePath = "https://image.tmdb.org/t/p/original";
   const data = await fetch(`https://api.themoviedb.org/3/movie/${params.movie}?api_key=${process.env.API_KEY}`);
   const res = await data.json();
-  const {id, title, release_date, poster_path } = res;
-  console.log(res);
+
   return (
-    <div>
-      <h1>Movie detail</h1>
-      <h1>{title}</h1>
-            <h3>{release_date}</h3>
-                <Image
-                    src={imagePath + poster_path}
-                    alt={title}
-                    width={800}
-                    height={1200}
-                />
+    <div className={`bg-[url('${imagePath + res.backdrop_path}')]`}>
+      <h2 className="text-2xl">{res.title}</h2>
+      <h2 className="text-lg">{res.release_date}</h2>
+      <h2>Runtime: {res.runtime} minutes</h2>
+      <h2 className="text-sm bg-green-600 inline-block my-2 px-2 py-1 rounded-lg border-2 border-green-400"><b>{res.status}</b></h2>
+      <Image
+        className="easeload my-12 w-full border-2"
+        src={imagePath + res.backdrop_path}
+        width={800}
+        height={450}
+        priority
+      />
+      <p>{res.overview}</p>
     </div>
   )
 }
