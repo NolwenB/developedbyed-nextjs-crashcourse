d825ea852da21c4b55c6b6fd60341af7420e9e44
Nolwen <nbachtle@outlook.com>
2023-02-21 21:37:52 -0500

    Added generateStaticParams()

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,9 +1,22 @@
 import Image from "next/image"
 
+export async function generateStaticParams() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+    {next: {revalidate: 60}},
+    );
+  const res = await data.json();
+  return res.results.map((movie) => ({
+    movie: toString(movie.id),
+  }));
+}
+
 export default async function MovieDetail({params}) {
   console.log(params);
   const imagePath = "https://image.tmdb.org/t/p/original";
-  const data = await fetch(`https://api.themoviedb.org/3/movie/${params.movie}?api_key=${process.env.API_KEY}`);
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/${params.movie}?api_key=${process.env.API_KEY}`,
+    );
   const res = await data.json();
 
   return (
